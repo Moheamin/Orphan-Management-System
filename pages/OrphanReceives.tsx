0c1ab881f07e2a06922834108a4f7680d5a8e73f
moheamin852 <moheamin852@gmail.com>
2026-05-10 03:22:07 +0300

    .feat: Add description to orphan recevies page

--- pages/OrphanReceives.tsx
+++ pages/OrphanReceives.tsx
@@ -1,3 +1,4 @@
+import "../src/index.css";
 import { useMemo, useState } from "react";
 import { useGetOrphanReceives } from "../utils/ReactQuerry/OrphanReceives/useGetOrphanReceives";
 import { DataTable } from "../components/CompoundTable";
@@ -402,7 +403,23 @@ function OrphanReceivesContent() {
   );
 }
 
-export default function OrphanReceivesTable() {
+export default function OrphanReceives() {
+  return (
+    <div className="mx-4 md:mx-8 mb-8">
+      <div className="mt-6 md:mt-8 mr-2 mb-6 md:mb-8 flex flex-col items-end">
+        <h1 className="mb-2 text-xl md:text-2xl font-bold text-[var(--textColor)]">
+          إستلامات الأيتام
+        </h1>
+        <p className="mb-4 text-[var(--subTextColor)] text-sm">
+          عرض تفاصيل استقبالات الأيتام من الكفلاء والدار
+        </p>
+      </div>
+      <OrphanReceivesTable />
+    </div>
+  );
+}
+
+function OrphanReceivesTable() {
   return (
     <DataTable.Root>
       <OrphanReceivesContent />
